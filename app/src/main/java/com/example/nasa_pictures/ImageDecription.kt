70150b670328ef8ea7f8c0a22523780c75cb1e22
abhinesh <abhinesh.c@eegrab.com>
2021-03-24 12:08:11 +0530

    able to show image in other screen

--- app/src/main/java/com/example/nasa_pictures/ImageDecription.kt
+++ app/src/main/java/com/example/nasa_pictures/ImageDecription.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.nasa_pictures.ImageDetails.ImageDetails
 import kotlinx.android.synthetic.main.fragment_image_description.*
+import kotlinx.android.synthetic.main.recycler_item.view.*
 
 
 class ImageDecription : Fragment(R.layout.fragment_image_description) {
@@ -17,7 +19,7 @@ class ImageDecription : Fragment(R.layout.fragment_image_description) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         tvTitle.text = args.ImageDetails.title
-
+        Glide.with(context).load(args.ImageDetails.url).into(ivSecondFrag);
     }
 
 }
