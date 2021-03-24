b1dcdb59ead68c75791d22452f488948e7ac30b8
abhineshchandra1234 <52635220+abhineshchandra1234@users.noreply.github.com>
2021-03-24 07:27:40 +0530

    successfully able to fetch json data inside fragment from asset

--- app/src/main/java/com/example/nasa_pictures/MainActivity.kt
+++ app/src/main/java/com/example/nasa_pictures/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.nasa_pictures
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
